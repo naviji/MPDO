85865de1a0088b49992f271aca22bee11082dd36
cyrign <cyrign@cse.iitk.ac.in>
2020-05-09 15:01:03 +0530

    Co-authored-by: naveenmv@iitk.ac.in <naveenmv@iitk.ac.in>

--- program.py
+++ program.py
@@ -1,4 +1,22 @@
 from z3 import *
+import random
+import sys
+
+class Obstacle:
+    moves = [(0,0), (1,0), (0,1), (-1,0), (0,-1)]
+
+    def __init__(self, x, y, grid):
+        self.x = x
+        self.y = y
+        self.grid = grid
+    
+    def next_move(self):
+        while (True):
+            dx, dy = random.choice(self.moves)
+            if ((0 <= self.x+dx < self.grid) and (0 <= self.y+dy < self.grid)):
+                self.x += dx
+                self.y += dy
+                return (self.x, self.y)
 
 def intersection_points(robot_pos, obs_pos):
     (rx, ry) = robot_pos
@@ -9,110 +27,130 @@ def intersection_points(robot_pos, obs_pos):
     return list(robot_halo.intersection(obs_halo))
 
 def get_plan(m):
-    
-    # sorted([i.split('_') for i in l], key=lambda item: int(item[1]))
     return sorted([d.name() for d in m.decls() if m[d]==True], key=lambda item: int(item.split('_')[1]))
 
-def get_robot_pos(m,time):
-    (_, _, x, y) = get_plan(m)[time].split('_') 
+def get_robot_pos(m,hop):
+    (_, _, x, y) = get_plan(m)[hop].split('_') 
     return (int(x), int(y)) 
+
+def path_valid(robot_plan, obs_plan):
+    return len([(a, b) for a, b in list(zip(robot_plan, obs_plan)) if a == b]) == 0
     
-GRID_SZ = 4
-HOPS = 10
+GRID_SZ = 6
+HOPS = int(GRID_SZ*1.825)
 
-OBS_MOVEMENT = [
-      (0, 3)
-      , (0, 3)
-      , (0, 3)
-      , (0, 3)
-      , (0, 2)
-      , (0, 2)
-      , (0, 3)
-      , (0, 3)
-      , (0, 3)
-      , (0, 3)
-      , (0, 3)
-]
+print("WORKSPACE SIZE (%s x %s)" % (GRID_SZ, GRID_SZ))
+print("HOPS ALLOWED = %s" % (HOPS))
 
-# X is a three dimensional grid containing (t, x, y)
-X =  [ [ [ Bool("x_%s_%s_%s" % (k, i, j)) for j in range(GRID_SZ) ]
-      for i in range(GRID_SZ) ] 
-      for k in range(HOPS+1)]
+def main():
+    # print(args)
+    # seed = int(args[0])
+    # random.seed(seed)
 
-s = Solver()
-
-# Initial Constraints
-s.add(X[0][0][0])
-s.add([Not(cell) for row in X[0] for cell in row][1:])
-
-# Final constraints
-s.add(X[HOPS][GRID_SZ-1][GRID_SZ-1])
-s.add([Not(cell) for row in X[HOPS] for cell in row][:-1])
-
-#Sanity Constraints
-for grid in X:
-    for i in range(len(grid)):
-        for j in range(len(grid)):
-            for p in range(len(grid)):
-                for q in range(len(grid)):
-                    if not (i==p and j==q):
-                        s.add(Not(And(grid[i][j], grid[p][q])))
-
-
-#Motion primitives
-
-for t in range(HOPS):
-      for x in range(GRID_SZ):
-            for y in range(GRID_SZ):
-                temp = Or(X[t][x][y])
-                if (x+1 < GRID_SZ):
-                    temp = Or(temp, X[t][x+1][y])
-                if (y+1 < GRID_SZ):
-                    temp = Or(temp, X[t][x][y+1])
-                if (x-1 >= 0):
-                    temp = Or(temp, X[t][x-1][y])
-                if (y-1 >= 0):
-                    temp = Or(temp, X[t][x][y-1])
-                s.add(simplify(Implies(X[t+1][x][y], temp)))
-
-
-########################################################
-
-time = 0
-s.check()
-
-
-# m = s.model()
-
-
-while (time < HOPS):
     
-    robot_pos = (0, 0) if time == 0 else get_robot_pos(m,time)
-    obs_pos = OBS_MOVEMENT[time]
-    s.add(X[time][robot_pos[0]][robot_pos[1]])
-    print("time is ", time)
-    print("robot at ", robot_pos)
-    print("obs at ", obs_pos)
+    # X is a three dimensional grid containing (t, x, y)
+    X =  [ [ [ Bool("x_%s_%s_%s" % (k, i, j)) for j in range(GRID_SZ) ]
+        for i in range(GRID_SZ) ] 
+        for k in range(HOPS+1)]
+
+    s = Solver()
+
+    # Initial Constraints
+    s.add(X[0][0][0])
+    s.add([Not(cell) for row in X[0] for cell in row][1:])
+
+    # Final constraints
+    s.add(X[HOPS][GRID_SZ-1][GRID_SZ-1])
+    s.add([Not(cell) for row in X[HOPS] for cell in row][:-1])
+
+    #Sanity Constraints
+    for grid in X:
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                for p in range(len(grid)):
+                    for q in range(len(grid)):
+                        if not (i==p and j==q):
+                            s.add(Not(And(grid[i][j], grid[p][q])))
     
-    s.push()
-    print("intersection points")
-    print(intersection_points(robot_pos, obs_pos))
-    
-    for (x, y) in intersection_points(robot_pos, obs_pos):
-        s.add(Not(X[time+1][x][y]))
-    
-    if s.check() == unsat:
-        print("stay there")
-    else:
-        m = s.model()
-        print("Plan for time = " + str(time+1))
-        print(get_plan(m))
+    #Motion primitives
+    for t in range(HOPS):
+        for x in range(GRID_SZ):
+                for y in range(GRID_SZ):
+                    temp = Or(X[t][x][y])
+                    if (x+1 < GRID_SZ):
+                        temp = Or(temp, X[t][x+1][y])
+                    if (y+1 < GRID_SZ):
+                        temp = Or(temp, X[t][x][y+1])
+                    if (x-1 >= 0):
+                        temp = Or(temp, X[t][x-1][y])
+                    if (y-1 >= 0):
+                        temp = Or(temp, X[t][x][y-1])
+                    s.add(simplify(Implies(X[t+1][x][y], temp)))
+
+    hop = 0
+    s.check()
+
+    m = s.model()
+
+    obs1 = Obstacle(0, 3, GRID_SZ)
+
+    robot_plan = []
+    obs_plan = []
+    while (hop < HOPS):
         
-    s.pop()
-    time += 1
+        robot_pos = (0, 0) if hop == 0 else get_robot_pos(m,hop)
+        obs_pos = obs1.next_move()
+        
+        s.add(X[hop][robot_pos[0]][robot_pos[1]])
+        # print("hop is ", hop)
+        # print("robot at ", robot_pos)
+        # print("obs at ", obs_pos)
 
-robot_pos = get_robot_pos(m,time)
-obs_pos = OBS_MOVEMENT[time]
-print("time is ", time)
-print("robot at ", robot_pos)
-print("obs at ", obs_pos)
+        if robot_pos == obs_pos:
+            print("COLLISION!!!")
+            print(robot_plan)
+            print(obs_plan)
+            exit()
+
+        robot_plan.append(robot_pos)
+        obs_plan.append(obs_pos)
+        
+        s.push()
+        # print("intersection points")
+        # print(intersection_points(robot_pos, obs_pos))
+        
+        for (x, y) in intersection_points(robot_pos, obs_pos):
+            #consider only the intersection with the next step in the plan
+            s.add(Not(X[hop+1][x][y]))
+        
+        if s.check() == unsat:
+            print("stay there")
+        else:
+            m = s.model()
+            # print("Plan for hop = " + str(hop+1))
+            # print(get_plan(m))
+            hop += 1
+
+        s.pop()
+        
+    robot_pos = get_robot_pos(m,hop)
+    obs_pos = obs1.next_move()
+    # print("hop is ", hop)
+    # print("robot at ", robot_pos)
+    # print("obs at ", obs_pos)
+    robot_plan.append(robot_pos)
+    obs_plan.append(obs_pos)
+
+    if path_valid(robot_plan, obs_plan):
+        print("PATH IS VALID!!!")
+    else:
+        print("PATH IS INVALID!!!")
+    print("ROBOT MOVEMENT:")
+    print(robot_plan)
+    print("OBSTACLE MOVEMENT:")
+    print(obs_plan)
+
+if __name__ == "__main__":
+    # print(sys.argv)
+    # main(sys.argv[1:])
+    main()
